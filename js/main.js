b4e3d557fafe33083ba8925688f4d6f9df47d403
Cristiane Mayara <cris.mayara@gmail.com>
2018-03-24 17:31:16 -0300

    Calculating imc value for patients

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var titulo = document;
+
+console.log(document);
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+  var paciente = pacientes[i];
+
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
+
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
+
+  var tdImc = paciente.querySelector(".info-imc");
+  var imc = peso / (altura * altura);
+  tdImc.textContent = imc.toFixed(2);
+}
